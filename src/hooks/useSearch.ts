09d9364e0fd8ade9ae3649fb6a2b392a66776c67
VitaHsu <vitahsu7@gmail.com>
2023-10-30 18:05:41 +0800

    moidfy useSearch func

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,8 +1,17 @@
 import { useContext } from 'react'
 import { SearchContext } from '../contexts'
 
-export default function useSearch<T extends Partial<T>>(): Search | T {
+export default function useSearch<T extends Partial<T>>(): [Search | T, (search: Search) => void] {
   const search = useContext(SearchContext).search
+  const setSearch = (search: Search) => {
+    const { search: _search, index } = window.router
+    _search[index] = search
 
-  return search
+    window.router = {
+      ...window.router,
+      search: _search
+    }
+  }
+
+  return [search, setSearch]
 }
