13d63f658b54b8bbb0092a1b4df261b72657fb13
VitaHsu <vitahsu7@gmail.com>
2023-10-30 21:17:04 +0800

    modifty useSearch.setSearch

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,15 +1,36 @@
 import { useContext } from 'react'
 import { SearchContext } from '../contexts'
 
+interface SetSearchOptions {
+  replace?: boolean
+}
+
 export default function useSearch<T extends Partial<T>>(): [Search | T, (search: Search) => void] {
   const search = useContext(SearchContext).search
-  const setSearch = (search: Search) => {
-    const { search: _search, index } = window.router
-    _search[index] = search
 
-    window.router = {
-      ...window.router,
-      search: _search
+  const setSearch = (search: Search, options: SetSearchOptions = {}) => {
+    const { history, pathname, search: _search, index } = window.router
+    const { replace } = options
+
+    if (replace) {
+      _search[index] = search
+
+      window.router = {
+        pathname,
+        history,
+        index,
+        search: _search
+      }
+    } else {
+      _search.push(search)
+      history.push(pathname)
+
+      window.router = {
+        pathname,
+        history,
+        index: history.length - 1,
+        search: _search
+      }
     }
   }
 
